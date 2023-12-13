30909b56506c5e9856f3c2666e02fc0025a592fc
OriMeiraz <ori.meiraz@campus.technion.ac.il>
2023-12-13 23:23:39 +0200

    now prints both

--- cyber.py
+++ cyber.py
@@ -2,5 +2,10 @@ def print_hello_world():
     print("Hello World")
 
 
+def print_hello_cyber():
+    print("Hello Cyber")
+
+
 if __name__ == "__main__":
     print_hello_world()
+    print_hello_cyber()
