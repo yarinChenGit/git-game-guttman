01f095b82fb226c06c458e4996aab3c0d0cdbcfd
OriMeiraz <ori.meiraz@campus.technion.ac.il>
2023-12-13 23:25:45 +0200

    created knn branch

---
+++ cyber.py
@@ -0,0 +1,11 @@
+def print_hello_world():
+    print("Hello Cyber")
+
+
+def print_hello_cyber():
+    print("Hello World")
+
+
+if __name__ == "__main__":
+    print_hello_world()
+    print_hello_cyber()
